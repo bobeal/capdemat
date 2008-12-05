755ea9b35638ad6bef32e697292ce91c3c782e10
Victor Bartel <vba@zenexity.fr>
2008-12-05 17:47:17 +0100

    Front office documents page finalisation. Addition of related translations.

--- BackOfficeNG/modules/frontoffice/controllers/DocumentController.groovy
+++ BackOfficeNG/modules/frontoffice/controllers/DocumentController.groovy
@@ -1,4 +1,4 @@
-import fr.cg95.cvq.business.document.DocumentTypeimport fr.cg95.cvq.business.document.DocumentStateimport java.lang.Objectimport java.lang.Stringimport java.util.Hashtableimport fr.cg95.cvq.business.users.Adult
+import fr.cg95.cvq.business.users.Individualimport fr.cg95.cvq.service.document.IDocumentTypeServiceimport fr.cg95.cvq.business.document.DocumentTypeimport fr.cg95.cvq.business.document.DocumentStateimport java.lang.Objectimport java.lang.Stringimport java.util.Hashtableimport fr.cg95.cvq.business.users.Adult
 import grails.converters.JSON
 import fr.cg95.cvq.security.SecurityContext
 import fr.cg95.cvq.service.users.IHomeFolderService
@@ -9,29 +9,28 @@ class DocumentController {
     
     IHomeFolderService homeFolderService
     IDocumentService documentService
-    
+    IDocumentTypeService documentTypeService    InstructionService instructionService    
     Adult currentEcitizen
-    
+    int maxRows    
     def beforeInterceptor = {
-        currentEcitizen = SecurityContext.getCurrentEcitizen()
-    }
-
+        currentEcitizen = SecurityContext.getCurrentEcitizen()        maxRows = 10
+    }    
     def index = {
         def state = [:]
-        def requests = [:]        def individuals = []        
-        if (params.ps) state = JSON.parse(params.ps)                currentEcitizen.homeFolder.individuals.each{            individuals.add([                id : it.id,                fullName : "${it?.firstName} ${it?.lastName}"            ])        }
-                
+        def requests = [:]        
+        if (params.ps) state = JSON.parse(params.ps)        if (params.df != null) state.df = params.df;        if (params.nf != null) state.nf = params.nf;        if (params.sf != null) state.sf = params.sf;
+        
         return ([
             'state': state,            'documents' : this.getDocuments(state,params),
-            'pageState' : (new JSON(state)).toString(),            'individuals' : individuals,            'states': DocumentState.allDocumentStates.collect{it.toString().toLowerCase()},
-        ]);
+            'pageState' : (new JSON(state)).toString(),            'individuals' : this.getIndividuals(),            'states': DocumentState.allDocumentStates.collect{it.toString().toLowerCase()},            'types' : this.getTypes(),            'maxRows': maxRows
+        ])
     }
     
-    def protected getDocuments = {state,params ->
-        def criterias = new Hashtable<String,Object>();        
+    def protected getDocuments = {state,params ->        def result = []
+        def criterias = new Hashtable<String,Object>();        int offset = !params?.offset ? 0 : Integer.parseInt(params.offset)                        if(state?.df) criterias.put("documentType.id",Long.valueOf(state.df))        if(state?.sf) criterias.put("state",DocumentState.forString(StringUtils.firstCase(state.sf,'')))        if(state?.nf == currentEcitizen.homeFolder.id.toString())             criterias.put('homeFolderId',Long.valueOf(state.nf))        else if(state?.nf)            criterias.put('individualId',Long.valueOf(state.nf))                this.documentService.search(criterias,maxRows,offset).each{            Individual sbj;            if(it?.individualId != null) sbj = this.individualService.getById(it.individualId)            result.add([                'id' : it.id,                'creationDate' : it.creationDate,                'certified' : it.certified,                'endValidityDate' : it.endValidityDate,                'state' : it.state.toString(),                'subject' : sbj?.firstName,                'name' : it.documentType.name,                'depositType' : it.depositType,                'depositOrigin' : it.depositOrigin,                'depositor' : this.instructionService.getActionPosterDetails(it.depositId),                'title' : message(code: CapdematUtils.adaptDocumentTypeName(it.documentType.name))            ]);        }
         
         return [
-            'all' : this.documentService.search(criterias),            'count' : this.documentService.searchCount(criterias)
+            'all' : result,            'count' : this.documentService.searchCount(criterias)
         ]
-    }
+    }        def protected getTypes = {        def result = []        this.documentTypeService.allDocumentTypes.each{            result.add([                id: it.id,                name: message(code:CapdematUtils.adaptDocumentTypeName(it.name))            ])        }        return result    }        def protected getIndividuals = {        def individuals = []                currentEcitizen.homeFolder.individuals.each{            individuals.add([                id : it.id,                fullName : "${it?.firstName}"            ])        }        individuals.add([            id:currentEcitizen.homeFolder.id,            fullName:message(code:'property.commonDocuments')        ])        return individuals    }
 }
---
+++ BackOfficeNG/modules/frontoffice/views/document/_documentList.gsp
@@ -0,0 +1,50 @@
+<g:if test="${documents?.all?.size() > 0}">
+  <ul>
+    <g:each var="record" in="${documents.all}">
+      <li>
+        <p>
+          <g:capdematEnumToFlag var="${record.state}" i18nKeyPrefix="document.state" />
+          <g:if test="${record.certified}">
+            <span class="certified">
+              <g:message code="property.certified" />
+            </span>
+          </g:if>
+          <span>
+            ${record.title}
+            <g:if test="${record?.subject}">
+               (${record.subject})
+            </g:if> 
+          </span>
+          <g:if test="${record?.endValidityDate}">
+            <g:message code="message.expireOn" />
+            <span>
+              ${format('value':record.endValidityDate,'formatName':'format.date')}
+            </span>
+          </g:if>
+        </p>
+        <p>
+          <g:if test="${record?.creationDate || record?.depositor}">
+            <span>
+              <g:message code="message.created" />
+              <g:if test="${record?.creationDate}">
+                <g:message code="message.date.on" />
+                ${format('value':record.creationDate,'formatName':'format.date')}
+              </g:if>
+              <g:if test="${record?.depositor}">
+                <g:message code="message.by" />
+                ${record.depositor?.firstName} ${record.depositor?.lastName}
+              </g:if>
+            </span>
+          </g:if>
+          <g:capdematEnumToFlag var="${record.depositType}" i18nKeyPrefix="document.depositType" />
+          <g:capdematEnumToFlag var="${record.depositOrigin}" i18nKeyPrefix="document.depositOrigin" />
+        </p>
+      </li>
+    </g:each>
+  </ul>
+</g:if>
+<g:else>
+  <strong>
+    <g:message code="message.noDocuments" />
+  </strong>
+</g:else>

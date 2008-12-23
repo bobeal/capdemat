4f9e41ae12bc5a4cc41f49e079bab5804ba1895e
Benoit Orihuela <bor@zenexity.fr>
2008-12-23 15:20:38 +0100

    added missing grails service

---
+++ BackOfficeNG/grails-app/services/RequestAdaptorService.groovy
@@ -0,0 +1,25 @@
+class RequestAdaptorService {
+    
+    def instructionService;
+    def translationService;
+    
+    def prepareRecords = { requests ->
+        if (!requests?.records) requests.records = []
+        requests.all.each {
+            requests.records.add([
+                'id':it.id,
+                'label':translationService.getEncodedRequestTypeLabelTranslation(it.requestType.label),
+                'creationDate':it.creationDate,
+                'requesterLastName':it.requesterLastName,
+                'requesterFirstName': it.requesterFirstName,
+                'subjectLastName':it.subjectLastName,
+                'subjectFirstName': it.subjectFirstName,
+                'state':it.state.toString(),
+                'lastModificationDate':it.lastModificationDate,
+                'lastInterveningAgentId': instructionService.getActionPosterDetails(it.lastInterveningAgentId) ,
+            ]);
+        }
+        
+        return requests
+    }
+}

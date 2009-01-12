ca8bfcea327505817c3a07efa38c172f11ea198a
Rafik Djedjig <rdj@zenexity.fr>
2009-01-12 14:06:49 +0100

    Exctracting i18n properties files generation code in I18nPlugin.java

---
+++ Generator/src/java/fr/cg95/cvq/generator/plugins/i18n/RequestI18n.java
@@ -0,0 +1,55 @@
+package fr.cg95.cvq.generator.plugins.i18n;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+
+import fr.cg95.cvq.generator.common.Step;
+
+/**
+ * @author rdj@zenexity.fr
+ */
+public class RequestI18n {
+
+    private String acronym;
+    private Map<String,Map<String,String>> i18nLabels = new HashMap<String, Map<String,String>>();
+    private  List<Step> steps;
+    
+
+    public RequestI18n(String targetNamespace) {
+        this.acronym = StringUtils.substringAfterLast(targetNamespace, "/");
+    }
+    
+    public List<Step> getSteps() {
+        return steps;
+    }
+    public void setSteps(List<Step> steps) {
+        this.steps = new ArrayList<Step>(steps);
+        
+        for (Iterator<Step> it = this.steps.iterator(); it.hasNext();) {
+            if (it.next().getName() == null)
+                it.remove();
+        }
+    }
+    
+    public String getAcronym() {
+        return acronym;
+    }
+    
+    public Map<String, Map<String,String>> getI18nLabels() {
+        return i18nLabels;
+    }
+    
+    public void addI18nLabel(String lang, String descType, String label) {
+        if (i18nLabels.get(lang) == null) {
+            Map<String, String> map = new HashMap<String, String>();
+            i18nLabels.put(lang, map);
+        }
+        i18nLabels.get(lang).put(descType, label);
+    }
+    
+}

6ae849bf03d2067c25c50b349005d085a29b9baf
Rafik Djedjig <rdj@zenexity.fr>
2009-02-11 12:09:54 +0100

    Refactor js code, to conform to Capdemat editable list pattern

--- BackOfficeNG/web-app/js/backoffice/categoryAgents.js
+++ BackOfficeNG/web-app/js/backoffice/categoryAgents.js
@@ -1,174 +1,140 @@
-/**
- * categrory"s agent management
- * display , add, del
- */
-
-zenexity.capdemat.bong.categoryAgent = function() {
-  
+(function(){
   var zcb = zenexity.capdemat.bong;
   var zcc = zenexity.capdemat.common;
   var zct = zenexity.capdemat.tools;
   var yud = YAHOO.util.Dom;
-  var yuel = YAHOO.util.Element;
+  var yu = YAHOO.util;
   var yue = YAHOO.util.Event;
   var yus = YAHOO.util.Selector;
   var ylj = YAHOO.lang.JSON;
+
+  zcb.categoryAgent = function() {
   
-  /* display */
-  var handleViewAgentsSuccess =  function(o) {
-    if (o.argument[0] === "All") {
-      yud.addClass("viewAllAgentsLink", "current");
-      yud.removeClass("viewCategoryAgentsLink", "current");
-    } else {
-      yud.removeClass("viewAllAgentsLink", "current");
-      yud.addClass("viewCategoryAgentsLink", "current");
-    }
-    yud.get("categoryAgents").innerHTML = o.responseText;
-  }
-      
-  function viewAgents(scope) {
-    zct.doAjaxCall(
-        "/agents/?id=" + zcb.categoryId + "&scope=" + scope,
-        [scope],
-        handleViewAgentsSuccess);
-  }
-  
-  function sortAgents() {
-    if (yus.query("select[name=orderAgentBy]", "sortAgentForm", true).value != "")
-      zct.doAjaxFormSubmitCall ("sortAgentForm", ["All"], handleViewAgentsSuccess);
-  }
-  
-  /* update current editing agent profile */
-  function updateAgentProfile(agentId, action) {
-    var currentLiEl = new yuel("agent_" + agentId);
-    
-    var profileSpanEls = yus.query("li[id|=agent] > span[class|=tag]", "agent_" + agentId);
-    if  (profileSpanEls.length > 0) {
-      currentLiEl.removeChild(profileSpanEls[0]);
+    var displayAgents =  function(o) {
+      if (o.argument[0] === "All") {
+        yud.addClass("viewAllAgentsLink", "current");
+        yud.removeClass("viewCategoryAgentsLink", "current");
+      } else {
+        yud.removeClass("viewAllAgentsLink", "current");
+        yud.addClass("viewCategoryAgentsLink", "current");
+      }
+      yud.get("categoryAgents").innerHTML = o.responseText;
     }
     
-    if (action != "unassociate") {
-      var cloneNewProfileSpanEl = yud.getPreviousSibling(
-        yus.query("input:checked", "agentEditForm_" + agentId, true)
-      ).cloneNode(true);
-      
-      currentLiEl.appendChild(cloneNewProfileSpanEl);
+    function getAgentId(e) {
+      return yud.getAncestorByTagName(yue.getTarget(e), 'li').id.replace("agent_", "");
     }
-  }
-  
-  /* cancel editable list item form */
-  function cancelAgentEditForm(agentId) {
-    new yuel("agent_" + agentId).removeChild(yud.get("agentEditForm_" + agentId));
+    
+    function cancelAgentEditForm(agentId) {
+      new yu.Element("agent_" + agentId).removeChild(yud.get("agentEditForm_" + agentId));
+      // re-active add/edit item link button
+      yud.removeClass(yus.query("a.currentEditItem", "agent_" + agentId, true), "currentEditItem");
+    }
+    
+    //FIXME - simplify agent display update policy by ajax refresh for example
+    function updateAgentProfile(agentId, action) {
+      var currentLiEl = new yu.Element("agent_" + agentId);
+       
+      var profileSpanEls = yus.query("li[id|=agent] > span[class|=tag]", "agent_" + agentId);
+      if  (profileSpanEls.length > 0)
+        currentLiEl.removeChild(profileSpanEls[0]);
       
-    // re-active add/edit item link button
-    yud.removeClass(
-      yus.query("a.currentEditItem", "agent_" + agentId, true),
-      "currentEditItem");
-  }
-  
-  /* submit editable list item form */
-  function submitAgentEditForm(agentId) {
-    zct.doAjaxFormSubmitCall(
-        "agentEditForm_" + agentId,
-        null, 
-        function(o) {
-          var response = ylj.parse(o.responseText);
-          if (response.status === "ok") {
-            updateAgentProfile(agentId, "editItem");
-            // remove item edit form
-            cancelAgentEditForm(agentId);
-            // remove cssClass 'notbelong' if necessary
-            yud.removeClass("agent_" + agentId, "notBelong");
-            // add unassociate button (TODO: it may be dangeourous if anchor element order is changed)
-            var firstAnchorEl = yus.query("a", "agent_" + agentId, true);
-            if (firstAnchorEl.className === "associate") {
-              cloneAnchorEl =  firstAnchorEl.cloneNode(false);
-              yud.replaceClass(cloneAnchorEl, cloneAnchorEl.className, "unassociate");
-              new yuel(cloneAnchorEl)
-                .appendTo(yud.get("agent_" + agentId), firstAnchorEl);
-              
-              // transform "asociate button" in "editItem button"
-              yud.replaceClass(firstAnchorEl, firstAnchorEl.className, "editItem");
-            }
-            
-          } else {
-            zct.Notifier.processMessage('modelError',response.success_msg);
-          }
-        });
-  }
-  
-  /* view editable list item form */
-  function viewAgentEditForm(agentId, targetElClassName) {
-    zct.doAjaxCall(
-        "/editAgent/" + "?id=" + zcb.categoryId + "&agentId=" + agentId,
-        null,
-        function(o) {
+      if (action != "unassociate") {
+        var cloneNewProfileSpanEl = 
+          yud.getPreviousSibling(yus.query("input:checked", "agentEditForm_" + agentId, true)).cloneNode(true);
+        currentLiEl.appendChild(cloneNewProfileSpanEl);
+      }
+    }
+    
+    function displayAgentEditForm(e) {
+      var agentId = getAgentId(e);
+      zct.doAjaxCall("/editAgent/" + "?id=" + zcb.categoryId + "&agentId=" + agentId, null, function(o) {
           var currentLiEl = yud.get("agent_" + agentId);
           currentLiEl.innerHTML += o.responseText;
           // unactive edit item link button
-          yud.addClass(
-            yus.query("a." + targetElClassName, "agent_" + agentId, true),
-            "currentEditItem");          
-        });
-  }
-  
-  /* association */	
-  function unassociateAgentToCategory(agentId) {
-    var url = "/unassociateAgent/?categoryId=" + zcb.categoryId + "&agentId=" + agentId;
-    
-    zct.doAjaxCall(
-        url,
-        null,
-        function(o) {
-          var response = ylj.parse(o.responseText);
-          if (response.status === "ok") {
-            var currentLiEl = yud.get("agent_" + agentId);
-            yud.addClass(currentLiEl, "notBelong");
-            currentLiEl.removeChild(yus.query("a.unassociate", "agent_" + agentId, true));
-            yud.replaceClass(
-                yus.query("a.editItem", "agent_" + agentId, true),
-                "editItem",
-                "associate");
-            
-            updateAgentProfile(agentId, "unassociate");
-          } else {
-            zct.Notifier.processMessage('modelError',response.success_msg);
-          }
-        });
-  }
-  
-  yue.addListener(
-      "categoryAgents",
-      "click",
-      function (e) {
-        var targetEl = yue.getTarget(e);
-        var parentTargetEl = yue.getTarget(e).parentNode;
-        // TODO - implement an ignore case policy to test tagname
-        var agentId = parentTargetEl.tagName === "LI" ?
-          agentId = parentTargetEl.id.replace("agent_", "")
-          : agentId = parentTargetEl.parentNode.id.replace("agent_", "");
-        
-        if (yud.hasClass(targetEl, "associate") && !yud.hasClass(targetEl, "currentEditItem"))
-          viewAgentEditForm(agentId, "associate");
-        
-        else if (yud.hasClass(targetEl, "unassociate"))
-          unassociateAgentToCategory(agentId);
-        
-        else if (yud.hasClass(targetEl, "editItem") && !yud.hasClass(targetEl, "currentEditItem"))
-          viewAgentEditForm(agentId, "editItem");
-        
-        else if (yud.hasClass(targetEl, "submitEditItem")) {
-          if (FIC_checkForm(e, yud.get("agentEditForm_" + agentId + "Errors")))
-            submitAgentEditForm(agentId);
-        }
-        
-        else if (yud.hasClass(targetEl, "cancelEditItem"))
-          cancelAgentEditForm(agentId);
+          yud.addClass(yus.query("a." + yue.getTargetEl().className, "agent_" + agentId, true), "currentEditItem");          
       });
-     
-  return {
-    viewAgents : function(scope) { viewAgents(scope); },
-    sortAgents : function() { sortAgents(); }
-  };
+    }
 
-}();
+    return {
+      clickEvent : undefined,
+      
+      init: function() {
+          zcb.categoryAgent.clickEvent = new zct.Event(zcb.categoryAgent, zcb.categoryAgent.getHandler);
+          yue.on('categoryAgents','click', zcb.categoryAgent.clickEvent.dispatch,
+              zcb.categoryAgent.clickEvent, true);
+      },
+      
+      getHandler : function(e) {
+          return yue.getTarget(e).className.split(' ')[0];
+      },
+      
+      associate : function(e) {
+          displayAgentEditForm(e);
+      },
+      
+      unassociate : function(e) {
+          var agentId = getAgentId(e);
+          var url = "/unassociateAgent/?categoryId=" + zcb.categoryId + "&agentId=" + agentId;
+          zct.doAjaxCall(url, null, function(o) {
+              var response = ylj.parse(o.responseText);
+              if (response.status === "ok") {
+                var currentLiEl = yud.get("agent_" + agentId);
+                yud.addClass(currentLiEl, "notBelong");
+                currentLiEl.removeChild(yus.query("a.unassociate", "agent_" + agentId, true));
+                yud.replaceClass(yus.query("a.editItem", "agent_" + agentId, true), "editItem", "associate");
+                updateAgentProfile(agentId, "unassociate");
+              } else
+                zct.Notifier.processMessage('modelError',response.success_msg);
+          });
+      },
+      
+      editItem : function(e) {
+          displayAgentEditForm(e);
+      },
+      
+      submitEditItem : function(e) {
+          var agentId = getAgentId(e);
+          if (!FIC_checkForm(e, yud.get("agentEditForm_" + agentId + "Errors")))
+            return;
+          zct.doAjaxFormSubmitCall("agentEditForm_" + agentId, null, function(o) {
+              var response = ylj.parse(o.responseText);
+              if (response.status === "ok") {
+                updateAgentProfile(agentId, "editItem");
+                // remove item edit form
+                cancelAgentEditForm(agentId);
+                // remove cssClass 'notbelong' if necessary
+                yud.removeClass("agent_" + agentId, "notBelong");
+                // add unassociate button (TODO: it may be dangeourous if anchor element order is changed)
+                var firstAnchorEl = yus.query("a", "agent_" + agentId, true);
+                if (firstAnchorEl.className === "associate") {
+                  cloneAnchorEl =  firstAnchorEl.cloneNode(false);
+                  yud.replaceClass(cloneAnchorEl, cloneAnchorEl.className, "unassociate");
+                  new yu.Element(cloneAnchorEl).appendTo(yud.get("agent_" + agentId), firstAnchorEl);
+                  // transform "asociate button" in "editItem button"
+                  yud.replaceClass(firstAnchorEl, firstAnchorEl.className, "editItem");
+                }
+              } else
+                zct.Notifier.processMessage('modelError',response.success_msg);
+        });
+      },
+      
+      cancelEditItem : function(e) {
+          cancelAgentEditForm(getAgentId(e));
+      },
+      
+      viewAgents : function(scope) {
+          zct.doAjaxCall("/agents/?id=" + zcb.categoryId + "&scope=" + scope, [scope], displayAgents);
+      },
+      // TODO - no more used - remove from code
+      sortAgents : function() {
+          if (yus.query("select[name=orderAgentBy]", "sortAgentForm", true).value != "")
+            zct.doAjaxFormSubmitCall ("sortAgentForm", ["All"], displayAgents);
+      }
+    };
+
+  }();
+  
+  YAHOO.util.Event.onDOMReady(zcb.categoryAgent.init);
+  
+}());
--- BackOfficeNG/web-app/js/backoffice/categoryRequests.js
+++ BackOfficeNG/web-app/js/backoffice/categoryRequests.js
@@ -1,10 +1,4 @@
-/**
- * categrory request types management
- * sort display , add, del
- */
-
-zenexity.capdemat.bong.categoryRequestType = function() {
-  
+(function(){
   var zcb = zenexity.capdemat.bong;
   var zcc = zenexity.capdemat.common;
   var zct = zenexity.capdemat.tools;
@@ -12,78 +6,86 @@ zenexity.capdemat.bong.categoryRequestType = function() {
   var yue = YAHOO.util.Event;
   var yus = YAHOO.util.Selector;
   var ylj = YAHOO.lang.JSON;
-  
-  /* display , sort */
-  var handleViewRequestTypesSuccess = function(o) {
-    if (o.argument[0] === "All") {
-      yud.addClass("viewAllRequestTypesLink", "current");
-      yud.removeClass("viewCategoryRequestTypesLink", "current");
-    } else {
-      yud.removeClass("viewAllRequestTypesLink", "current");
-      yud.addClass("viewCategoryRequestTypesLink", "current");
-    }
-    yud.get("categoryRequestTypes").innerHTML = o.responseText;
-  }
-  
-  function viewRequestTypes(scope) {
-    // set the scope manually in the form in order to get it from the POSTed parameters
-    yud.get("scope").value = scope;
-    zct.doAjaxFormSubmitCall ("sortRequestTypeForm", [scope], handleViewRequestTypesSuccess);
-  }
-  
-  function sortRequestTypes() {
-    if (yus.query("select[name=orderRequestTypeBy]", "sortRequestTypeForm", true).value != "")
-      zct.doAjaxFormSubmitCall ("sortRequestTypeForm", [yud.get("scope").value], handleViewRequestTypesSuccess);
-  }
-  
-  /* association */	
-  function associateRequestTypeToCategory(requestTypeId, action) {
-    var url = "";
-    
-    url += action === "associate" ? "/associateRequestType/" : "/unassociateRequestType/";
-    url += "?categoryId=" + zcb.categoryId +  "&requestTypeId=" + requestTypeId;
-    
-    zct.doAjaxCall(
-      url,null,
-      function(o) {
-        var response = ylj.parse(o.responseText);
-        if (response.status === "ok") {
-          var currentLiEl = yud.get("requestType_" + requestTypeId);
-          var targetAnchorEl = yus.query("a." + action, "requestType_" + requestTypeId, true);    
-          var categorySpanEl = yus.query("span.categoryName", "requestType_" + requestTypeId, true);
-              
-          if (action === "associate") {
-            yud.removeClass(currentLiEl, "notBelong");
-            yud.replaceClass(targetAnchorEl, action, "unassociate");
-            if (categorySpanEl != null) {
-              // TODO : it may be better to remove node
-              yud.replaceClass(categorySpanEl, "categoryName", "invisible");
-            }
-          } else {
-            yud.addClass(currentLiEl, "notBelong");
-            yud.replaceClass(targetAnchorEl, action, "associate");
-          } 
-        } else {
-          zct.Notifier.processMessage('modelError',response.success_msg);
-        }  
-      });
-  }
-  
-  yue.addListener(
-      "categoryRequestTypes",
-      "click",
-      function(e) {
-        var targetEl = yue.getTarget(e);
-        if (targetEl.className === "associate" || targetEl.className === "unassociate" ) {
-          associateRequestTypeToCategory(
-            targetEl.parentNode.id.replace("requestType_", ""),
-            targetEl.className);
-        }
-      });
-  
-  return {
-    viewRequestTypes : function(scope) { viewRequestTypes(scope); },
-    sortRequestTypes : function() { sortRequestTypes(); }
-  };
 
-}();
+  zcb.categoryRequestType = function() {
+  
+    var displayRequestTypes = function(o) {
+      if (o.argument[0] === "All") {
+        yud.addClass("viewAllRequestTypesLink", "current");
+        yud.removeClass("viewCategoryRequestTypesLink", "current");
+      } else {
+        yud.removeClass("viewAllRequestTypesLink", "current");
+        yud.addClass("viewCategoryRequestTypesLink", "current");
+      }
+      yud.get("categoryRequestTypes").innerHTML = o.responseText;
+    }
+    
+    function manageRequestType(targetEl) {
+      var requestTypeId = targetEl.parentNode.id.replace("requestType_", "");
+      var action = targetEl.className;
+      var url = "";
+      
+      url += action === "associate" ? "/associateRequestType/" : "/unassociateRequestType/";
+      url += "?categoryId=" + zcb.categoryId +  "&requestTypeId=" + requestTypeId;
+      
+      zct.doAjaxCall(url, null, function(o) {
+          var response = ylj.parse(o.responseText);
+          if (response.status === "ok") {
+            var currentLiEl = yud.get("requestType_" + requestTypeId);
+            var targetAnchorEl = yus.query("a." + action, "requestType_" + requestTypeId, true);    
+            var categorySpanEl = yus.query("span.categoryName", "requestType_" + requestTypeId, true);
+                
+            if (action === "associate") {
+              yud.removeClass(currentLiEl, "notBelong");
+              yud.replaceClass(targetAnchorEl, action, "unassociate");
+              if (categorySpanEl != null) {
+                // TODO : it may be better to remove node
+                yud.replaceClass(categorySpanEl, "categoryName", "invisible");
+              }
+            } else {
+              yud.addClass(currentLiEl, "notBelong");
+              yud.replaceClass(targetAnchorEl, action, "associate");
+            } 
+          } else {
+            zct.Notifier.processMessage('modelError',response.success_msg);
+          }  
+        });
+    }
+
+    return {
+      clickEvent : undefined,
+      
+      init: function() {
+          zcb.categoryRequestType.clickEvent = new zct.Event(zcb.categoryRequestType, zcb.categoryRequestType.getHandler);
+          yue.on('categoryRequestTypes','click', zcb.categoryRequestType.clickEvent.dispatch,
+              zcb.categoryRequestType.clickEvent, true);
+      },
+      
+      getHandler : function(e) {
+          return yue.getTarget(e).className.split(' ')[0];
+      },
+      
+      associate : function(e) {
+          manageRequestType(yue.getTarget(e));
+      },
+      
+      unassociate : function(e) {
+          manageRequestType(yue.getTarget(e));
+      },
+      
+      viewRequestTypes : function(scope) {
+          yud.get("scope").value = scope;
+          zct.doAjaxFormSubmitCall ("sortRequestTypeForm", [scope], displayRequestTypes);
+      },
+      
+      sortRequestTypes : function() {
+          if (yus.query("select[name=orderRequestTypeBy]", "sortRequestTypeForm", true).value != "")
+            zct.doAjaxFormSubmitCall ("sortRequestTypeForm", [yud.get("scope").value], displayRequestTypes);
+      }
+    };
+
+  }();
+  
+  YAHOO.util.Event.onDOMReady(zcb.categoryRequestType.init);
+  
+}());

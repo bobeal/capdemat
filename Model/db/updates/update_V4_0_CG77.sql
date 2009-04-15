1c4ed74f824e6951fab701fcbc3f2abe3f2ea878
Benoit Orihuela <bor@zenexity.fr>
2009-04-15 14:15:40 +0200

    removed non un-necessary column renaming

--- Model/db/updates/update_V4_0_CG77.sql
+++ Model/db/updates/update_V4_0_CG77.sql
@@ -68,5 +68,3 @@ DELETE FROM request_type where label = 'Study Grant Request';
 -- DELETE FROM request_action where request_id in (SELECT id from request where request_type_id = (select id from request_type where label = 'Study Grant'));
 -- DELETE FROM request where id in (SELECT id from request where request_type_id = (select id from request_type where label = 'Study Grant'));
 
-alter table study_grant_request rename column a_levels to alevels;
-alter table study_grant_request rename column a_levels_date to alevels_date;

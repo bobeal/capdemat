1f0f37a0d4b45cdcf657a5bbc5501b0c223d4ea4
Jean-Sébastien Bour <jsb@zenexity.fr>
2009-04-24 11:41:16 +0200

    Fixed typo in SQL update script

--- Model/db/updates/update_V4_0_LA-CUSTOMIZATION.sql
+++ Model/db/updates/update_V4_0_LA-CUSTOMIZATION.sql
@@ -1,4 +1,4 @@
-alter table local_authority add column display_title varchar(100)
+alter table local_authority add column display_title varchar(100);
 update local_authority set display_title=name;
 alter table local_authority alter column display_title set not null;
 

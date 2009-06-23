608c642fae8b38c6090198298087be29fccd5ff8
Jean-Sébastien Bour <jsb@zenexity.fr>
2009-06-23 17:01:49 +0200

    Create subkeys when needed for existing traces

--- Model/db/updates/update_V4_0_CG77.sql
+++ Model/db/updates/update_V4_0_CG77.sql
@@ -133,3 +133,4 @@ alter table external_service_traces alter column key type character varying(255)
 alter table study_grant_request add column account_holder_edemande_id varchar(255);
 
 alter table external_service_traces add column subkey varchar(255);
+update external_service_traces set subkey = 'subject' where message like '%tiers%';

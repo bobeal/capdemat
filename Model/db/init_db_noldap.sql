7c4968fda17a4eac882e65a12e378f95ee588e7f
Benoit Orihuela <bor@zenexity.fr>
2009-07-14 19:58:58 +0200

    Some fixes for quickstart mode and v4.0 release preparation

--- Model/db/init_db_noldap.sql
+++ Model/db/init_db_noldap.sql
@@ -1,9 +1,10 @@
-INSERT INTO agent VALUES (nextval('hibernate_sequence'), 'admin');
-INSERT INTO agent_site_roles (agent_id, local_authority_id, profile) VALUES ( (select id from agent where login = 'admin'), (select max(id) from local_authority), 'Admin');
+INSERT INTO agent (id, login, last_name, first_name, active) VALUES (nextval('hibernate_sequence'), 'admin', 'administrateur', '', 'true');
+INSERT INTO agent_site_roles (agent_id, profile) VALUES ( (select id from agent where login = 'admin'), 'Admin');
 
-INSERT INTO agent VALUES (nextval('hibernate_sequence'), 'agent');
-INSERT INTO agent_site_roles (agent_id, local_authority_id, profile) VALUES ( (select id from agent where login = 'agent'), (select max(id) from local_authority), 'None');
+INSERT INTO agent (id, login, last_name, first_name, active) VALUES (nextval('hibernate_sequence'), 'agent', 'agent', '', 'true');
+INSERT INTO agent_site_roles (agent_id, profile) VALUES ( (select id from agent where login = 'agent'), 'None');
 
-INSERT INTO school VALUES (nextval('hibernate_sequence'), 'Ecole de la ville', NULL, 't', (select max(id) from local_authority));
+INSERT INTO school VALUES (nextval('hibernate_sequence'), 'Ecole de la ville', NULL, 't');
 
-INSERT INTO recreation_center VALUES (nextval('hibernate_sequence'), 'Centre de loisirs de la ville', (select max(id) from local_authority), NULL, 't');
+INSERT INTO recreation_center VALUES (nextval('hibernate_sequence'), 'Centre de loisirs de la ville', NULL, 't');
+

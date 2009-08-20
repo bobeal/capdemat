a874be78a7a6d1d9ef027f3c33dbcee46a34fd6f
Jean-Sébastien Bour <jsb@zenexity.fr>
2009-08-20 11:38:37 +0200

    Add a test for concurrent locking

--- Model/test/java/fr/cg95/cvq/service/request/RequestServiceTest.java
+++ Model/test/java/fr/cg95/cvq/service/request/RequestServiceTest.java
@@ -20,6 +20,7 @@ import fr.cg95.cvq.business.users.Adult;
 import fr.cg95.cvq.business.users.CreationBean;
 import fr.cg95.cvq.exception.CvqException;
 import fr.cg95.cvq.exception.CvqModelException;
+import fr.cg95.cvq.security.PermissionException;
 import fr.cg95.cvq.security.SecurityContext;
 import fr.cg95.cvq.testtool.ServiceTestCase;
 import fr.cg95.cvq.util.Critere;
@@ -253,5 +254,76 @@ public class RequestServiceTest extends ServiceTestCase {
                 RequestFormType.REQUEST_MAIL_TEMPLATE);
             Assert.assertEquals(0, forms.size());
         }
+    }
+
+    public void testRequestLocks() throws CvqException {
+        // create a home folder request
+        CreationBean creationBean = gimmeAnHomeFolder();
+        continueWithNewTransaction();
+        Long requestId = creationBean.getRequestId();
+        Request request;
+        // try to lock it with agent method in FO
+        try {
+            iRequestService.getAndTryToLock(requestId);
+            fail("should have been forbidden");
+        } catch (PermissionException e) {
+            // OK
+        } catch (Exception e) {
+            fail("should have thrown a PermissionException");
+        }
+        // lock it with ecitizen method
+        request = iRequestService.getAndLock(requestId);
+        assertEquals(requestId, request.getId());
+        assertTrue(iRequestService.isLockedByCurrentUser(requestId));
+        assertFalse(iRequestService.isLocked(requestId));
+        // try to lock it in BO
+        SecurityContext.setCurrentSite(localAuthorityName, SecurityContext.BACK_OFFICE_CONTEXT);
+        SecurityContext.setCurrentAgent(agentNameWithCategoriesRoles);
+        request = iRequestService.getAndTryToLock(requestId);
+        // check it couldn't be locked
+        assertEquals(requestId, request.getId());
+        assertFalse(iRequestService.isLockedByCurrentUser(requestId));
+        assertTrue(iRequestService.isLocked(requestId));
+        // go back in FO and release request
+        SecurityContext.setCurrentSite(localAuthorityName, SecurityContext.FRONT_OFFICE_CONTEXT);
+        SecurityContext.setCurrentEcitizen(creationBean.getLogin());
+        iRequestService.release(requestId);
+        // check it was correctly released
+        assertFalse(iRequestService.isLockedByCurrentUser(requestId));
+        assertFalse(iRequestService.isLocked(requestId));
+        // go lock it in BO
+        SecurityContext.setCurrentSite(localAuthorityName, SecurityContext.BACK_OFFICE_CONTEXT);
+        SecurityContext.setCurrentAgent(agentNameWithCategoriesRoles);
+        // try to lock it with ecitizen method in BO
+        try {
+            iRequestService.getAndLock(requestId);
+            fail("should have been forbidden");
+        } catch (PermissionException e) {
+            // OK
+        } catch (Exception e) {
+            fail("should have thrown a PermissionException");
+        }
+        // lock it with correct method
+        request = iRequestService.getAndTryToLock(requestId);
+        assertEquals(requestId, request.getId());
+        assertTrue(iRequestService.isLockedByCurrentUser(requestId));
+        assertFalse(iRequestService.isLocked(requestId));
+        // go back in FO and try to lock it
+        SecurityContext.setCurrentSite(localAuthorityName, SecurityContext.FRONT_OFFICE_CONTEXT);
+        SecurityContext.setCurrentEcitizen(creationBean.getLogin());
+        try {
+            iRequestService.getAndLock(requestId);
+            fail("should have been forbidden");
+        } catch (CvqException e) {
+            // OK
+        }  catch (Exception e) {
+            fail("should have thrown a CvqException");
+        }
+        // go back in BO and release it
+        SecurityContext.setCurrentSite(localAuthorityName, SecurityContext.BACK_OFFICE_CONTEXT);
+        SecurityContext.setCurrentAgent(agentNameWithCategoriesRoles);
+        iRequestService.release(requestId);
+        assertFalse(iRequestService.isLockedByCurrentUser(requestId));
+        assertFalse(iRequestService.isLocked(requestId));
     }
 }

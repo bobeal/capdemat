9b608d29719fad7c0e09d5f87315c570b1565919
Rafik Djedjig <rdj@zenexity.fr>
2009-10-09 15:21:31 +0200

    Fix localRefrential widget js validation error enhancement bug

--- BackOfficeNG/web-app/js/common/validationRules.js
+++ BackOfficeNG/web-app/js/common/validationRules.js
@@ -2,10 +2,13 @@
   var zct = zenexity.capdemat.tools;
   var zcv = zenexity.capdemat.Validation;
   var yl = YAHOO.lang;
+  var yud = YAHOO.util.Dom;
 
   zcv.putRules({
     'cfbn': new zcv.rule('regex', /^[0-9]{7}[A-Z]{0,1}$/),
     'localReferentialData': new zcv.rule('func', function(f, key){
+      var newEnhanceErrorEl = yud.getAncestorByClassName(f.enhanceErrorEl,'dataTree');
+      if (!!newEnhanceErrorEl) f.enhanceErrorEl = newEnhanceErrorEl;
       for (name in zcv.fields)
         if (key.split('[')[0] === name.split('[')[0] && zcv.fields[name].checked)
           return true;

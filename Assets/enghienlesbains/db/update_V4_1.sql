8c3b42107846e66ba31670b7eeaaf4280c2f7967
Benoit Orihuela <bor@zenexity.fr>
2009-12-15 14:12:05 +0100

    Updated DB name and boostrapped SQL migration script for enghien

---
+++ Assets/enghienlesbains/db/update_V4_1.sql
@@ -0,0 +1,3 @@
+update local_authority set postal_code = '95880', display_title = 'Enghien-les-Bains', 
+    instruction_alerts_enabled = 'false', instruction_alerts_detailed = 'true',
+    instruction_default_max_delay = 10, instruction_default_alert_delay = 3;

fb163f902a6273dda31b0620d2aab2118f64ae1d
Rafik Djedjig <rdj@zenexity.fr>
2009-12-21 17:34:40 +0100

    Migrate franconville asset - bootstrap db migration script

---
+++ Assets/franconville/db/update_V4_1.sql
@@ -0,0 +1,4 @@
+update local_authority set postal_code = '95130', display_title = 'Franconvilles', 
+    instruction_alerts_enabled = 'false', instruction_alerts_detailed = 'false',
+    instruction_default_max_delay = 10, instruction_default_alert_delay = 3;
+

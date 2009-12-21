eb74d13e82ad3e9fd6d7641aa325a726f687dd4d
Rafik Djedjig <rdj@zenexity.fr>
2009-12-21 17:32:57 +0100

    Migrate vetheuil asset - bootstrap db migration script

---
+++ Assets/vetheuil/db/update_V4_1.sql
@@ -0,0 +1,4 @@
+update local_authority set postal_code = '95510', display_title = 'Vétheuil', 
+    instruction_alerts_enabled = 'false', instruction_alerts_detailed = 'true',
+    instruction_default_max_delay = 10, instruction_default_alert_delay = 3;
+

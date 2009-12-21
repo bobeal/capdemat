ac36e0ccdb6d7be16d98b0cc85507753c6686484
Benoit Orihuela <bor@zenexity.fr>
2009-12-21 10:43:26 +0100

    Minor cleanup

--- Assets/MigrationCt.groovy
+++ Assets/MigrationCt.groovy
@@ -8,7 +8,7 @@ def cssFoFileWriter = new FileWriter('css/cssFo.css')
 new File('../valdoise/css/cssFo.css').eachLine { line ->
     // first is menu color
     // second is body and menu hover colors
-    cssFoFileWriter.write(line.replaceAll('#ff812b','#75a0c3').replaceAll('#b6aca5','#eee'))
+    cssFoFileWriter.write(line.replaceAll('#ff812b','#26866A').replaceAll('#b6aca5','#eee'))
     cssFoFileWriter.write('\n')
 }
 cssFoFileWriter.flush()
@@ -23,21 +23,20 @@ cssFoFileWriter.flush()
 /* img directory */
 ["img/banner.jpg":"img/logoFo.png","img/logoBO.jpg":"img/logoBo.png","xsl/xsllogo.jpg":"img/logoPdf.png"].each { k,v ->
     System.err.println("reading " + k)
-    File input = new File(k);
-    BufferedImage image = ImageIO.read(input);
-    File output = new File(v); 
-    ImageIO.write(image, "png", output);
-    "git rm $k".execute()
-    "git add $v".execute()
+	try {
+        File input = new File(k);
+        BufferedImage image = ImageIO.read(input);
+        File output = new File(v); 
+        ImageIO.write(image, "png", output);
+        "git rm $k".execute()
+        "git add $v".execute()
+	} catch (Exception e) {
+	    System.err.println("error while transforming")
+	}
 }
 
-/* local_referential directory */
-
 // delete old local_referential_cvq.xml
-"git rm local_referential/local_referential_cvq.xml".execute()
-
-// remove obsolete attributes from others
-// TODO : enghien has none of them
+// "git rm local_referential/local_referential_cvq.xml".execute()
 
 /* xsl directory */
 "mkdir -p html/templates/mails".execute()

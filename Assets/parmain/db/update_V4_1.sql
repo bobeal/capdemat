b24d0f038176b9b7290d8b3a82a578b53cd36d39
Rafik Djedjig <rdj@zenexity.fr>
2009-12-21 17:34:12 +0100

    Migrate parmain asset - bootstrap db migration script

---
+++ Assets/parmain/db/update_V4_1.sql
@@ -0,0 +1,4 @@
+update local_authority set postal_code = '95620', display_title = 'Parmain', 
+    instruction_alerts_enabled = 'false', instruction_alerts_detailed = 'false',
+    instruction_default_max_delay = 10, instruction_default_alert_delay = 3;
+

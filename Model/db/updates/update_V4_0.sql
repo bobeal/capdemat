43d2885c6e11100ea7cce9432a5299c03b052df8
Jean-Sébastien Bour <jsb@zenexity.fr>
2010-01-05 14:44:48 +0100

    Remove associated request forms before removing request type

--- Model/db/updates/update_V4_0.sql
+++ Model/db/updates/update_V4_0.sql
@@ -94,6 +94,8 @@ drop table vacations_diary ;
 drop table vacations_registration_request_other_individual ;
 drop table vacations_registration_request ;
 delete from requirement where request_type_id = (select id from request_type where label = 'Vacations Registration');
+delete from request_form where id in (select request_form_id from forms where request_type_id = (select id from request_type where label = 'Vacations Registration'));
+delete from forms where request_type_id = (select id from request_type where label = 'Vacations Registration');
 delete from request_type where label = 'Vacations Registration';
 
 -- start of migration to indexed lists in requests collections elements

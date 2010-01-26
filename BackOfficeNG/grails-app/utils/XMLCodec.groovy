6d60129a5a08d95f47cf8aaf0585bbb5ec7a4380
Jean-Sébastien Bour <jsb@zenexity.fr>
2010-01-26 16:22:22 +0100

    Use str.toString() instead of str in XMLCodec, since Grails 1.2 tags return StreamCharBuffer instead of String instances

--- BackOfficeNG/grails-app/utils/XMLCodec.groovy
+++ BackOfficeNG/grails-app/utils/XMLCodec.groovy
@@ -3,10 +3,12 @@ import org.apache.commons.lang.StringEscapeUtils
 class XMLCodec {
 
     static encode = { str ->
-        return StringEscapeUtils.escapeXml(str)
+        if (str == null) return null
+        return StringEscapeUtils.escapeXml(str.toString())
     }
 
     static decode = {
-        return StringEscapeUtils.unescapeXml(str)
+        if (str == null) return null
+        return StringEscapeUtils.unescapeXml(str.toString())
     }
 }

e954e3de01dfdbce018e25bcc457522bcd41a637
Rafik Djedjig <rdj@zenexity.fr>
2010-02-04 18:05:30 +0100

    Set fallbackToSystemLocale to false to conform to i18n standard behaviour

--- BackOfficeNG/grails-app/conf/BootStrap.groovy
+++ BackOfficeNG/grails-app/conf/BootStrap.groovy
@@ -1,7 +1,14 @@
+import org.codehaus.groovy.grails.web.servlet.GrailsApplicationAttributes
+
 class BootStrap {
     
     def init = { servletContext ->
         servletContext.setAttribute("newDataBinder", GlobalPropertyEditorConfig.&newDataBinder)
+
+         // HACK: avoid default configuration of messageSource!!! (http://tramuntanal.wikidot.com/grails)
+        def appContext = servletContext.getAttribute(GrailsApplicationAttributes.APPLICATION_CONTEXT)
+        def messageSource = appContext.getBean("messageSource")
+        messageSource.fallbackToSystemLocale = false
     }
     
     def destroy = {

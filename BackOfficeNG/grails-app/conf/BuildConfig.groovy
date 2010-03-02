1be29e79762b122c6f1891d0d12809a49890d9e0
Benoit Orihuela <bor@zenexity.fr>
2010-03-02 09:37:38 +0100

    Cleaner way to remove 3.0.0 version of Spring jars from BONG war

--- BackOfficeNG/grails-app/conf/BuildConfig.groovy
+++ BackOfficeNG/grails-app/conf/BuildConfig.groovy
@@ -1,20 +1,15 @@
 grails.project.plugins.dir="./plugins"
 
 grails.war.resources = { stagingDir ->
-    delete(file:"${stagingDir}/WEB-INF/lib/org.springframework.aop-3.0.0.RELEASE.jar")
-    delete(file:"${stagingDir}/WEB-INF/lib/org.springframework.asm-3.0.0.RELEASE.jar")
-    delete(file:"${stagingDir}/WEB-INF/lib/org.springframework.aspects-3.0.0.RELEASE.jar")
-    delete(file:"${stagingDir}/WEB-INF/lib/org.springframework.beans-3.0.0.RELEASE.jar")
-    delete(file:"${stagingDir}/WEB-INF/lib/org.springframework.context-3.0.0.RELEASE.jar")
-    delete(file:"${stagingDir}/WEB-INF/lib/org.springframework.context.support-3.0.0.RELEASE.jar")
-    delete(file:"${stagingDir}/WEB-INF/lib/org.springframework.core-3.0.0.RELEASE.jar")
-    delete(file:"${stagingDir}/WEB-INF/lib/org.springframework.expression-3.0.0.RELEASE.jar")
-    delete(file:"${stagingDir}/WEB-INF/lib/org.springframework.instrument-3.0.0.RELEASE.jar")
-    delete(file:"${stagingDir}/WEB-INF/lib/org.springframework.jdbc-3.0.0.RELEASE.jar")
-    delete(file:"${stagingDir}/WEB-INF/lib/org.springframework.jms-3.0.0.RELEASE.jar")
-    delete(file:"${stagingDir}/WEB-INF/lib/org.springframework.orm-3.0.0.RELEASE.jar")
-    delete(file:"${stagingDir}/WEB-INF/lib/org.springframework.oxm-3.0.0.RELEASE.jar")
-    delete(file:"${stagingDir}/WEB-INF/lib/org.springframework.transaction-3.0.0.RELEASE.jar")
-    delete(file:"${stagingDir}/WEB-INF/lib/org.springframework.web-3.0.0.RELEASE.jar")
-    delete(file:"${stagingDir}/WEB-INF/lib/org.springframework.web.servlet-3.0.0.RELEASE.jar")
+    File libDir = new  File(stagingDir, 'WEB-INF/lib')
+    
+    def deleteJars = { jarNamePattern ->
+        libDir.eachFile { file ->
+            if (file.name.matches(jarNamePattern)) {
+                file.delete()
+            }
+        }
+    }
+
+    deleteJars java.util.regex.Pattern.compile('org.springframework.*3.0.0.*.jar') // conflicts with our version of spring framework
 }

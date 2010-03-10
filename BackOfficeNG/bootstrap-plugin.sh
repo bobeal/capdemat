0892008a84ce5615105b9781471e205580e05b6f
Jean-Sébastien Bour <jsb@zenexity.fr>
2010-03-10 14:08:58 +0100

    Set JCaptcha version to 1.0 (last compatible one with Grails 1.2.0)

--- BackOfficeNG/bootstrap-plugin.sh
+++ BackOfficeNG/bootstrap-plugin.sh
@@ -3,7 +3,7 @@ grails install-plugin google-chart
 
 grails install-plugin plugins/grails-capdemat-casclient-0.1.zip
 grails install-plugin plugins/grails-extended-data-binding-0.2.zip
-grails install-plugin jcaptcha
+grails install-plugin jcaptcha 1.0
 grails install-plugin webxml 1.2
 
 

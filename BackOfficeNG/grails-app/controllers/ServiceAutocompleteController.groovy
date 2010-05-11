2423a72944b1be3e1f7d0266d04bd7cf47380b8d
Sylvain MATHIEU <sma@zenexity.fr>
2010-05-11 18:25:36 +0200

    Configurable addresses referential web service url

--- BackOfficeNG/grails-app/controllers/ServiceAutocompleteController.groovy
+++ BackOfficeNG/grails-app/controllers/ServiceAutocompleteController.groovy
@@ -1,20 +1,27 @@
 import grails.converters.*
 import java.util.*
 import fr.cg95.cvq.security.*
+import org.codehaus.groovy.grails.commons.*
 
 class ServiceAutocompleteController {
 
     def cities = {
         def token = SecurityContext.getCurrentSite().token ?: ""
-        withHttp(uri: "http://localhost:9000") {
-           render get(path : '/cities', query : [search: params.search, postalCode: params.postalCode?:false, token:token]) as JSON
+        def url = ConfigurationHolder.config.addresses_referential_url
+        if(url) {
+            withHttp(uri: url) {
+               render get(path : '/cities', query : [search: params.search, postalCode: params.postalCode?:false, token:token]) as JSON
+            }
         }
     }
 
     def ways = {
         def token = SecurityContext.getCurrentSite().token ?: ""
-        withHttp(uri: "http://localhost:9000") {
-           render get(path : '/ways', query : [city: params.city, search: params.search, token:token]) as JSON
+        def url = ConfigurationHolder.config.addresses_referential_url
+        if(url) {
+            withHttp(uri: url) {
+               render get(path : '/ways', query : [city: params.city, search: params.search, token:token]) as JSON
+            }
         }
     }
 

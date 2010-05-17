25eacbe5e5123a0f4b20575f8b78096af34b2f11
Sylvain MATHIEU <sma@zenexity.fr>
2010-05-17 17:07:05 +0200

    Added token checking in FO to enable the autocomplete script

--- BackOfficeNG/grails-app/controllers/ServiceAutocompleteController.groovy
+++ BackOfficeNG/grails-app/controllers/ServiceAutocompleteController.groovy
@@ -5,12 +5,22 @@ import org.codehaus.groovy.grails.commons.*
 
 class ServiceAutocompleteController {
 
+    def tokenValidity = {
+        def token = SecurityContext.getCurrentSite().token ?: ""
+        def url = ConfigurationHolder.config.addresses_referential_url
+        if(url) {
+            withHttp(uri: url, contentType: groovyx.net.http.ContentType.JSON) {
+                render get(path: '/token', query: [token:token]) as JSON
+            }
+        }
+    }
+
     def cities = {
         def token = SecurityContext.getCurrentSite().token ?: ""
         def url = ConfigurationHolder.config.addresses_referential_url
         if(url) {
             withHttp(uri: url) {
-               render get(path : '/cities', query : [search: params.search, postalCode: params.postalCode?:false, token:token]) as JSON
+                render get(path: '/cities', query: [search: params.search, postalCode: params.postalCode?:false, token:token]) as JSON
             }
         }
     }
@@ -20,7 +30,7 @@ class ServiceAutocompleteController {
         def url = ConfigurationHolder.config.addresses_referential_url
         if(url) {
             withHttp(uri: url) {
-               render get(path : '/ways', query : [city: params.city, search: params.search, token:token]) as JSON
+                render get(path: '/ways', query: [city: params.city, search: params.search, token:token]) as JSON
             }
         }
     }
--- BackOfficeNG/web-app/js/frontoffice/addressAutocomplete.js
+++ BackOfficeNG/web-app/js/frontoffice/addressAutocomplete.js
@@ -7,77 +7,84 @@ zenexity.capdemat.tools.namespace("zenexity.capdemat.fong");
   var zct = zenexity.capdemat.tools;
   var yue = YAHOO.util.Event;
   var yus = YAHOO.util.Selector;
+  var yuc = YAHOO.util.Connect;
 
   zcf.AddressAutocomplete = function() {
 
+    var autocompleteInit = function() {
+      zct.each(yus.query(".address-fieldset"), function() {
+        var fieldsetId = this.id;
+        var streetNameAutocomplete;
+        if(document.getElementById(fieldsetId + "_streetName")) {
+          streetNameAutocomplete = new zcc.AutoComplete({
+            inputId: fieldsetId + "_streetName",
+            modalId: fieldsetId + "_streetName_autocomplete",
+            url: "/CapDemat/autocomplete/ways",
+            idField: "matriculation",
+            minimumChars: 2,
+            resultText: function(result) {
+              return result.name;
+            },
+            inputValue: function(result) {
+              return result.name;
+            },
+            onSelectedResult: function(result) {
+              document.getElementById(fieldsetId + "_streetMatriculation").value = result.matriculation;
+            }
+          });
+        }
+        if(document.getElementById(fieldsetId + "_city")) {
+          new zcc.AutoComplete({
+            inputId: fieldsetId + "_city",
+            modalId: fieldsetId + "_city_autocomplete",
+            url: "/CapDemat/autocomplete/cities",
+            idField: "inseeCode",
+            resultText: function(result) {
+              return result.postalCode + " " + result.name;
+            },
+            inputValue: function(result) {
+              return result.name;
+            },
+            onSelectedResult: function(result) {
+              document.getElementById(fieldsetId + "_postalCode").value = result.postalCode;
+              streetNameAutocomplete.urlParams.city = result.inseeCode;
+              document.getElementById(fieldsetId + "_cityInseeCode").value = result.inseeCode;
+              document.getElementById(fieldsetId + "_streetName").value = "";
+              document.getElementById(fieldsetId + "_streetMatriculation").value = "";
+            }
+          });
+        }
+        if(document.getElementById(fieldsetId + "_postalCode")) {
+          new zcc.AutoComplete({
+            inputId: fieldsetId + "_postalCode",
+            modalId: fieldsetId + "_postalCode_autocomplete",
+            url: "/CapDemat/autocomplete/cities",
+            urlParams: { postalCode: true },
+            idField: "inseeCode",
+            minimumChars: 2,
+            resultText: function(result) {
+              return result.postalCode + " " + result.name;
+            },
+            inputValue: function(result) {
+              return result.postalCode;
+            },
+            onSelectedResult: function(result) {
+              document.getElementById(fieldsetId + "_city").value = result.name;
+              streetNameAutocomplete.urlParams.city = result.inseeCode;
+              document.getElementById(fieldsetId + "_cityInseeCode").value = result.inseeCode;
+              document.getElementById(fieldsetId + "_streetName").value = "";
+              document.getElementById(fieldsetId + "_streetMatriculation").value = "";
+            }
+          });
+        }
+      });
+    };
+
     return {
       init: function() {
-        zct.each(yus.query(".address-fieldset"), function() {
-          var fieldsetId = this.id;
-          var streetNameAutocomplete;
-          if(document.getElementById(fieldsetId + "_streetName")) {
-            streetNameAutocomplete = new zcc.AutoComplete({
-              inputId: fieldsetId + "_streetName",
-              modalId: fieldsetId + "_streetName_autocomplete",
-              url: "/CapDemat/autocomplete/ways",
-              idField: "matriculation",
-              minimumChars: 2,
-              resultText: function(result) {
-                return result.name;
-              },
-              inputValue: function(result) {
-                return result.name;
-              },
-              onSelectedResult: function(result) {
-                document.getElementById(fieldsetId + "_streetMatriculation").value = result.matriculation;
-              }
-            });
-          }
-          if(document.getElementById(fieldsetId + "_city")) {
-            new zcc.AutoComplete({
-              inputId: fieldsetId + "_city",
-              modalId: fieldsetId + "_city_autocomplete",
-              url: "/CapDemat/autocomplete/cities",
-              idField: "inseeCode",
-              resultText: function(result) {
-                return result.postalCode + " " + result.name;
-              },
-              inputValue: function(result) {
-                return result.name;
-              },
-              onSelectedResult: function(result) {
-                document.getElementById(fieldsetId + "_postalCode").value = result.postalCode;
-                streetNameAutocomplete.urlParams.city = result.inseeCode;
-                document.getElementById(fieldsetId + "_cityInseeCode").value = result.inseeCode;
-                document.getElementById(fieldsetId + "_streetName").value = "";
-                document.getElementById(fieldsetId + "_streetMatriculation").value = "";
-              }
-            });
-          }
-          if(document.getElementById(fieldsetId + "_postalCode")) {
-            new zcc.AutoComplete({
-              inputId: fieldsetId + "_postalCode",
-              modalId: fieldsetId + "_postalCode_autocomplete",
-              url: "/CapDemat/autocomplete/cities",
-              urlParams: { postalCode: true },
-              idField: "inseeCode",
-              minimumChars: 2,
-              resultText: function(result) {
-                return result.postalCode + " " + result.name;
-              },
-              inputValue: function(result) {
-                return result.postalCode;
-              },
-              onSelectedResult: function(result) {
-                document.getElementById(fieldsetId + "_city").value = result.name;
-                streetNameAutocomplete.urlParams.city = result.inseeCode;
-                document.getElementById(fieldsetId + "_cityInseeCode").value = result.inseeCode;
-                document.getElementById(fieldsetId + "_streetName").value = "";
-                document.getElementById(fieldsetId + "_streetMatriculation").value = "";
-              }
-            });
-          }
-        });
+        yuc.asyncRequest("GET", "/CapDemat/autocomplete/tokenValidity", {
+          success: autocompleteInit
+        })
       }
     };
   }();

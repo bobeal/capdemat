4406925acb6bda92a6775c92fab0e378a6e27c68
Rafik Djedjig <rdj@zenexity.fr>
2010-06-16 12:09:06 +0200

    Patch PersistentStringEnum to support '_' char in text value

--- BackOfficeNG/src/groovy/PersistentStringEnumEditor.groovy
+++ BackOfficeNG/src/groovy/PersistentStringEnumEditor.groovy
@@ -3,9 +3,11 @@ import java.beans.PropertyEditorSupport
 class PersistentStringEnumEditor extends PropertyEditorSupport {
 
     public void setAsText(String text) {
-        def qualifiedEnumString = text.tokenize("_")
-        if (qualifiedEnumString[0] != null && qualifiedEnumString[0] != '')
-            this.value = Class.forName(qualifiedEnumString[0]).forString(qualifiedEnumString[1])
+        def qualifiedEnumString = text.tokenize('_')
+        def qualifiedClassName = qualifiedEnumString.remove(0)
+        def valueAsText = qualifiedEnumString.join('_')
+        if (qualifiedClassName != null && qualifiedClassName != '')
+            this.value = Class.forName(qualifiedClassName).forString(valueAsText)
         else
             this.value = null
     }

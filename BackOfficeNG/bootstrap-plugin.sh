3a2c1a0c53d57f31feed11d029547efad7bd60b4
Benoit Orihuela <bor@zenexity.fr>
2010-07-09 13:25:15 +0200

    Cleanup bootstrap-plugin.sh script (only keep our own plugins)

--- BackOfficeNG/bootstrap-plugin.sh
+++ BackOfficeNG/bootstrap-plugin.sh
@@ -1,9 +1,4 @@
-grails install-plugin google-chart
-#grails install-plugin extended-data-binding
-
 grails install-plugin plugins/grails-capdemat-casclient-0.1.zip
 grails install-plugin plugins/grails-extended-data-binding-0.2.zip
-grails install-plugin jcaptcha 1.0
-grails install-plugin webxml 1.2
 
 

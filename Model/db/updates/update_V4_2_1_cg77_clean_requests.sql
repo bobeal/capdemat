fcac14b4985df3edeb954f0bb0c9129865754080
Jean-Sébastien Bour <jsb@zenexity.fr>
2010-07-12 12:56:07 +0200

    Add some request cleaning to CG77 script

--- Model/db/updates/update_V4_2_1_cg77_clean_requests.sql
+++ Model/db/updates/update_V4_2_1_cg77_clean_requests.sql
@@ -8,3 +8,17 @@ delete from request_action where request_id in (4545, 534260, 714030, 1163513, 1
 delete from request_note where request_id in (4545, 534260, 714030, 1163513, 1593570, 4672671);
 delete from request_document where request_id in (4545, 534260, 714030, 1163513, 1593570, 4672671);
 delete from request where id in (4545, 534260, 714030, 1163513, 1593570, 4672671);
+
+update request set subject_id = 6673862 where id = 47495;
+update request set requester_id = 6673862 where id = 47495;
+update study_grant_request set current_school_name_precision = 'Texas A&amp;M University' where id = 2750;
+update study_grant_request set current_school_name_precision = 'CFA Utec I&amp;NT' where id = 14185;
+
+insert into local_referential_data (id, name) values (nextval('hibernate_sequence'), '121');
+update study_grant_request_tax_household_city set tax_household_city_id = currval('hibernate_sequence') where study_grant_request_id = 26300;
+
+insert into local_referential_data (id, name) values (nextval('hibernate_sequence'), '322');
+update study_grant_request_tax_household_city set tax_household_city_id = currval('hibernate_sequence') where study_grant_request_id = 20932;
+
+insert into local_referential_data (id, name) values (nextval('hibernate_sequence'), '333');
+update study_grant_request_tax_household_city set tax_household_city_id = currval('hibernate_sequence') where study_grant_request_id = 46485;

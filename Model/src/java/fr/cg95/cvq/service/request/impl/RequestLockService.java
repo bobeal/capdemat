561404c13dfca5065cb385e3f130f303491e2de0
Jean-Sébastien Bour <jsb@zenexity.fr>
2010-09-02 15:30:50 +0200

    Ticket #1305 : remove request locks cache in memory

--- Model/src/java/fr/cg95/cvq/service/request/impl/RequestLockService.java
+++ Model/src/java/fr/cg95/cvq/service/request/impl/RequestLockService.java
@@ -1,10 +1,6 @@
 package fr.cg95.cvq.service.request.impl;
 
-import java.util.Collections;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import org.joda.time.DateTime;
 import org.springframework.beans.BeansException;
@@ -13,6 +9,7 @@ import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.beans.factory.ListableBeanFactory;
 
 import fr.cg95.cvq.business.request.RequestLock;
+import fr.cg95.cvq.dao.hibernate.HibernateUtil;
 import fr.cg95.cvq.dao.request.IRequestDAO;
 import fr.cg95.cvq.exception.CvqException;
 import fr.cg95.cvq.security.PermissionException;
@@ -25,42 +22,35 @@ import fr.cg95.cvq.service.request.IRequestTypeService;
 
 public class RequestLockService implements IRequestLockService, BeanFactoryAware {
 
-    protected IRequestDAO requestDAO;
+    private IRequestDAO requestDAO;
     private IRequestTypeService requestTypeService;
     private ListableBeanFactory beanFactory;
-
-    private static Map<Long, RequestLock> locks =
-        Collections.synchronizedMap(new HashMap<Long, RequestLock>());
 
     @Override
     @Context(types = {ContextType.ECITIZEN}, privilege = ContextPrivilege.WRITE)
     public void lock(final Long id)
         throws CvqException {
-        synchronized(locks) {
-            applyLock(id);
-        }
+        applyLock(id);
     }
 
     @Override
     @Context(types = {ContextType.AGENT}, privilege = ContextPrivilege.READ)
     public void tryToLock(final Long id) {
-        synchronized(locks) {
-            try {
-                // FIXME JSB : hack to avoid bypassing aspect security
-                ((IRequestLockService)beanFactory.getBean("requestLockService")).applyLock(id);
-            } catch (PermissionException e) {
-                // couldn't lock request : we only have READ privilege
-            } catch (CvqException e) {
-                // couldn't lock request : it is probably already locked
-            }
+        try {
+            // FIXME JSB : hack to avoid bypassing aspect security
+            ((IRequestLockService)beanFactory.getBean("requestLockService")).applyLock(id);
+        } catch (PermissionException e) {
+            // couldn't lock request : we only have READ privilege
+        } catch (CvqException e) {
+            // couldn't lock request : it is probably already locked
         }
     }
 
     @Override
     @Context(types = {ContextType.ECITIZEN, ContextType.AGENT}, privilege = ContextPrivilege.WRITE)
     public void applyLock(final Long requestId)
-    throws CvqException {
-        synchronized(locks) {
+        throws CvqException {
+        synchronized(this) {
             RequestLock lock = getRequestLock(requestId);
             if (lock == null) {
                 // no lock, we can acquire a new one
@@ -78,18 +68,14 @@ public class RequestLockService implements IRequestLockService, BeanFactoryAware
                 throw new CvqException("request.lock.error.alreadyLocked");
             }
             requestDAO.saveOrUpdate(lock);
-            locks.put(requestId, lock);
+            HibernateUtil.getSession().flush();
         }
     }
 
     @Override
     @Context(types = {ContextType.ECITIZEN, ContextType.AGENT}, privilege = ContextPrivilege.READ)
     public RequestLock getRequestLock(final Long requestId) {
-        synchronized (locks) {
-            //check in memory cache
-            RequestLock lock = locks.get(requestId);
-            if (lock != null) return lock;
-            // check in DB
+        synchronized(this) {
             return requestDAO.getRequestLock(requestId);
         }
     }
@@ -97,52 +83,45 @@ public class RequestLockService implements IRequestLockService, BeanFactoryAware
     @Override
     @Context(types = {ContextType.ECITIZEN, ContextType.AGENT}, privilege = ContextPrivilege.READ)
     public boolean isLocked(final Long requestId) {
-        synchronized (locks) {
-            RequestLock lock = getRequestLock(requestId);
-            return (lock != null
-                    && !lock.getUserId().equals(SecurityContext.getCurrentUserId())
-                    && !new DateTime(lock.getDate().getTime()).plusMinutes(
-                        requestTypeService.getGlobalRequestTypeConfiguration().getRequestLockMaxDelay())
-                            .isBeforeNow());
-        }
+        RequestLock lock = getRequestLock(requestId);
+        return (lock != null
+                && !lock.getUserId().equals(SecurityContext.getCurrentUserId())
+                && !new DateTime(lock.getDate().getTime()).plusMinutes(
+                    requestTypeService.getGlobalRequestTypeConfiguration().getRequestLockMaxDelay())
+                        .isBeforeNow());
     }
 
     @Override
     @Context(types = {ContextType.ECITIZEN, ContextType.AGENT}, privilege = ContextPrivilege.READ)
     public boolean isLockedByCurrentUser(final Long requestId) {
-        synchronized (locks) {
-            RequestLock lock = getRequestLock(requestId);
-            return (lock != null
-                    && lock.getUserId().equals(SecurityContext.getCurrentUserId())
-                    && !new DateTime(lock.getDate().getTime()).plusMinutes(
-                        requestTypeService.getGlobalRequestTypeConfiguration().getRequestLockMaxDelay())
-                            .isBeforeNow());
-        }
+        RequestLock lock = getRequestLock(requestId);
+        return (lock != null
+                && lock.getUserId().equals(SecurityContext.getCurrentUserId())
+                && !new DateTime(lock.getDate().getTime()).plusMinutes(
+                    requestTypeService.getGlobalRequestTypeConfiguration().getRequestLockMaxDelay())
+                        .isBeforeNow());
     }
 
     @Override
     @Context(types = {ContextType.ECITIZEN, ContextType.AGENT}, privilege = ContextPrivilege.WRITE)
     public void release(final Long requestId) {
-        synchronized (locks) {
+        synchronized(this) {
             RequestLock lock = getRequestLock(requestId);
-            if (lock == null || !lock.getUserId()
-                    .equals(SecurityContext.getCurrentUserId())) {
+            if (lock == null || !lock.getUserId().equals(SecurityContext.getCurrentUserId())) {
                 return;
             }
             requestDAO.delete(lock);
-            locks.remove(requestId);
+            HibernateUtil.getSession().flush();
         }
     }
 
     @Override
     @Context(types = {ContextType.SUPER_ADMIN}, privilege = ContextPrivilege.NONE)
     public void cleanRequestLocks() {
-        synchronized (locks) {
-            List<Long> requestIds = requestDAO.cleanRequestLocks(
+        synchronized(this) {
+            requestDAO.cleanRequestLocks(
                 requestTypeService.getGlobalRequestTypeConfiguration().getRequestLockMaxDelay());
-            for (Long requestId : requestIds) {
-                locks.remove(requestId);
-            }
+            HibernateUtil.getSession().flush();
         }
     }
 

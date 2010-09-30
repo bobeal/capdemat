6a6b90612112f51469e6cbb4fd7fde2f4828b44c
Rafik Djedjig <rdj@zenexity.fr>
2010-09-30 17:59:50 +0200

    Add SP-PLUs service url mapping

--- BackOfficeNG/grails-app/conf/ServiceUrlMappings.groovy
+++ BackOfficeNG/grails-app/conf/ServiceUrlMappings.groovy
@@ -2,6 +2,6 @@ class ServiceUrlMappings {
   static mappings = {
 
     "/service/fakePayment/$action?/$id?" (controller : "serviceFakePayment" )
-
+    "/service/spplus/$action?/$id?" (controller : "serviceSpplus" )
   }
 }

d7465b075cf5018f10c54e120864ee9d1527d190
Julien Richard-Foy <jrf@zenexity.com>
2011-03-15 12:08:44 +0100

    [#609] Fix sql schema

--- Model/db/updates/update_V4_3.sql
+++ Model/db/updates/update_V4_3.sql
@@ -234,3 +234,5 @@ UPDATE child set child_born = true;
 -- End PESSAC contributions by Zenexity
 
 ALTER TABLE library_registration_request ADD COLUMN adult_content_authorization bool;
+
+ALTER TABLE school_canteen_registration_request ADD COLUMN which_food_allergy varchar(255);

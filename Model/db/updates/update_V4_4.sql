8ae2230c5cabd7d6a069e4a76da22aed5c6ccca7
Jean-Sébastien Bour <jsb@zenexity.fr>
2011-03-17 19:07:18 +0100

    Fix SQL update script for adress and boolean_is_child_born_i_love_javabeans renaming

--- Model/db/updates/update_V4_4.sql
+++ Model/db/updates/update_V4_4.sql
@@ -2,12 +2,14 @@ update home_folder set is_temporary = true where id in (select home_folder_id fr
 alter table request drop column has_tied_home_folder;
 
 alter table home_folder drop constraint FKDB87BBCE6AB1E860;
+alter table home_folder rename adress_id to address_id;
 alter table home_folder add constraint FKDB87BBCEB7531222 foreign key (address_id) references address;
 
 alter table individual drop constraint FKFD3DA2996AB1E860;
+alter table individual rename adress_id to address_id;
 alter table individual add constraint FKFD3DA299B7531222 foreign key (address_id) references address;
 
-alter table child alter column child_born rename to born;
+alter table child rename child_born to born;
 
 alter table document_action rename agent_id to user_id;
 alter table document_action rename label to type;

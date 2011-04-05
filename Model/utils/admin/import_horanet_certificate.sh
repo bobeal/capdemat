7a8f8c0ef521bbb63561298ac72c94110f07bacf
Benoit Orihuela <bor@zenexity.fr>
2011-04-05 17:43:30 +0200

    Remove absolutely deprecated admin scripts

--- Model/utils/admin/import_horanet_certificate.sh
+++
@@ -1,3 +0,0 @@
-# command line used to import Horanet certificate into our keystore file
-keytool -import -file horanet.cer -keystore cvq.keystore -storepass horanet
-
--- Model/utils/admin/start_plco.sh
+++
@@ -1,7 +0,0 @@
-# !/bin/sh
-#
-# Start the Franconville's PLCO payment server
-# by Benoit Orihuela
-
-cd /var/plco/franconville
-./startPLCO.sh

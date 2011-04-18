0e1e311bfa6f9b9b2478b59d374f509140433ceb
Benoit Orihuela <bobeal@gmail.com>
2011-04-18 23:25:06 +0200

    Fix V4.4's after migration script (request's note and document)

--- Model/db/updates/update_V4_4_after_migration.sql
+++ Model/db/updates/update_V4_4_after_migration.sql
@@ -2,6 +2,14 @@ delete from request_action where request_id in
     (select id from request where request_type_id in
         (select id from request_type where label = 'VO Card' or label = 'Home Folder Modification'));
 
+delete from request_note where request_id in 
+    (select id from request where request_type_id in
+        (select id from request_type where label = 'VO Card' or label = 'Home Folder Modification'));
+
+delete from request_document where request_id in 
+    (select id from request where request_type_id in
+        (select id from request_type where label = 'VO Card' or label = 'Home Folder Modification'));
+
 delete from home_folder_modification_request where id in
     (select specific_data_id from request where request_type_id in
         (select id from request_type where label = 'Home Folder Modification'));

bobeal/capdemat

commit 756a44ecde54f94cf90883776f880119ec5acc20

[Fix] https://code.zenexity.fr/issues/2246 PJs restent à l'état « Brouillon » après le passage de la demande de brouillon a en cours

Model/src/java/fr/cg95/cvq/service/request/impl/RequestWorkflowService.java
@@ -62,6 +62,7 @@ import fr.cg95.cvq.dao.request.IRequestDAO;
 import fr.cg95.cvq.exception.CvqException;
 import fr.cg95.cvq.exception.CvqInvalidTransitionException;
 import fr.cg95.cvq.exception.CvqModelException;
+import fr.cg95.cvq.exception.CvqObjectNotFoundException;
 import fr.cg95.cvq.exception.CvqValidationException;
 import fr.cg95.cvq.security.SecurityContext;
 import fr.cg95.cvq.security.annotation.Context;
@@ -800,7 +801,10 @@ public class RequestWorkflowService implements IRequestWorkflowService, Applicat
         Date date = new Date();
         updateLastModificationInformation(request, date);
 
+        /* Set all the states to pending */
         request.setState(RequestState.PENDING);
+        request.setDataState(DataState.PENDING);
+        updateDocumentsToPending(request);
 
         WorkflowPendingEvent wfEvent = new WorkflowPendingEvent(request);
         requestExternalService.publish(wfEvent);
@@ -808,6 +812,17 @@ public class RequestWorkflowService implements IRequestWorkflowService, Applicat
         byte[] pdfData = requestPdfService.generateCertificate(request);
         requestActionService.addCreationAction(request.getId(), date, pdfData, note);
         postActionsProcess(wfEvent.getWorkflowPostActions());
+    }
+
+    protected void updateDocumentsToPending(Request request) throws CvqException,
+            CvqObjectNotFoundException, CvqInvalidTransitionException {
+        Set<RequestDocument> documentSet = request.getDocuments();
+        if (documentSet == null)
+            return;
+
+        for (RequestDocument requestDocument : documentSet) {
+            documentService.pending(requestDocument.getDocumentId());
+        }
     }
 
     private void inProgress(Request request, final String note) throws CvqException {

commit 42637c384dc6458c001a52fa719046441cca6cb4

Fix regression introduced in 756a44ecde54f94cf90883776f880119ec5acc20

Model/src/java/fr/cg95/cvq/service/request/impl/RequestWorkflowService.java
@@ -25,6 +25,7 @@ import org.springframework.context.ApplicationListener;
 
 import fr.cg95.cvq.business.authority.LocalAuthorityResource;
 import fr.cg95.cvq.business.document.Document;
+import fr.cg95.cvq.business.document.DocumentState;
 import fr.cg95.cvq.business.request.DataState;
 import fr.cg95.cvq.business.request.Request;
 import fr.cg95.cvq.business.request.RequestActionType;
@@ -821,7 +822,9 @@ public class RequestWorkflowService implements IRequestWorkflowService, Applicat
             return;
 
         for (RequestDocument requestDocument : documentSet) {
-            documentService.pending(requestDocument.getDocumentId());
+            Document document = documentService.getById(requestDocument.getDocumentId());
+            if (document.getState().equals(DocumentState.DRAFT))
+                documentService.pending(requestDocument.getDocumentId());
         }
     }
 
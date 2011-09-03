6ad47b06d791971b6d70e0a26996c782b8616e8c
Benoit Orihuela <bor@zenexity.fr>
2011-09-03 19:05:09 +0200

    Add specific SQL instructions for dirty DBs

--- Model/db/updates/update_V4_6.sql
+++ Model/db/updates/update_V4_6.sql
@@ -6,3 +6,11 @@ alter table individual add column duplicate_alert bool;
 alter table individual add column duplicate_data TEXT;
 
 update individual set duplicate_alert = false;
+
+-- an old state found in old DB
+DELETE FROM request_action where resulting_state = 'Active';
+
+-- clean existing DBs : remove crap left by HFMRs
+delete from individual_role where owner_id is null;
+delete from individual_role where owner_id in (select id from individual where home_folder_id is null);
+

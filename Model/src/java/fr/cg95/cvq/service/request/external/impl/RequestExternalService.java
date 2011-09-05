bobeal/capdemat

commit 3cf923f6edcf79b41f7718242bfd0b5d4aedc43b

[Fix] Ensure external service is not unplugged before trying to access it

Model/src/java/fr/cg95/cvq/service/request/external/impl/RequestExternalService.java
@@ -17,7 +17,6 @@ import java.util.Map.Entry;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
-import org.apache.xmlbeans.XmlObject;
 import org.springframework.context.ApplicationListener;
 import org.springframework.scheduling.annotation.Async;
 
@@ -465,6 +464,11 @@ public class RequestExternalService extends ExternalService implements IRequestE
                         String externalServiceLabel = mapping.getExternalServiceLabel();
                         IExternalProviderService externalProviderService =
                             getExternalServiceByLabel(externalServiceLabel);
+                        if (externalProviderService == null) {
+                            logger.warn("onApplicationEvent() External service " 
+                                    + mapping.getExternalServiceLabel() + " is no longer existing");
+                            continue;
+                        }
                         if (externalProviderService instanceof ExternalApplicationProviderService)
                             continue;
 

commit 89ccd4a53fdb0fe69f4481b634186d3ea4e18e70

Fix missing import
 - Mistake introduced in 3cf923f6edcf79b41f7718242bfd0b5d4aedc43b

Model/src/java/fr/cg95/cvq/service/request/external/impl/RequestExternalService.java
@@ -17,6 +17,7 @@ import java.util.Map.Entry;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
+import org.apache.xmlbeans.XmlObject;
 import org.springframework.context.ApplicationListener;
 import org.springframework.scheduling.annotation.Async;
 
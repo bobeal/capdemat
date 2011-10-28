7000febf96b91a0a06945b6ffb66c653e3957e52
Florent Odier <fod@zenexity.com>
2011-10-28 12:00:52 +0200

    [Fix] Conditions on School Transport Registration
    Done in custom JS. #2575 can be closed.

--- BackOfficeNG/web-app/js/frontoffice/requesttype/schoolTransportRegistrationRequest.js
+++ BackOfficeNG/web-app/js/frontoffice/requesttype/schoolTransportRegistrationRequest.js
@@ -17,7 +17,7 @@ zenexity.capdemat.tools.namespace("zenexity.capdemat.fong.requesttype");
         var json = ylj.parse(o.responseText);
         var index = 1;
         for (var key in json) {
-            selector.options[index++] = new Option(json[key],key);
+          selector.options[index++] = new Option(json[key], key);
         }
         if (zct.isFunction(callback))
           callback.apply(selector, callbackParams);
@@ -59,7 +59,7 @@ zenexity.capdemat.tools.namespace("zenexity.capdemat.fong.requesttype");
       selector.id = id;
       selector.className = 'required validate-not-first';
       selector.name = id;
-      selector.options[0] = new Option('Choisissez...','');
+      selector.options[0] = new Option('Choisissez...', '');
       // Add methods
       selector.fill = fill;
       selector.empty = empty;
@@ -80,57 +80,101 @@ zenexity.capdemat.tools.namespace("zenexity.capdemat.fong.requesttype");
       });
     };
 
+    /**
+     * Dynamically fill selectors for the bus line and stop depending on the selected child.
+     */
+    var initTransports = function() {
+      var linesCallUrlPrefix = zenexity.capdemat.contextPath + '/frontoffice/schoolTransportRegistration/transportLines';
+      var stopsCallUrlPrefix = zenexity.capdemat.contextPath + '/frontoffice/schoolTransportRegistration/stops';
+
+      var subject = yud.get('subjectId');
+      var requestId = yud.get("stepForm").id.value;
+
+      changeLayout();
+
+      // If we come back from step 2, save last line and stop values.
+      var lastLineValue, lastStopValue;
+      if (subject.value !== '') {
+        lastLineValue = yud.get('idLigne').value;
+        lastStopValue = yud.get('idArret').value;
+      }
+
+      yud.get('idLigne').parentNode.replaceChild(new Selector('idLigne'), yud.get('idLigne'));
+      yud.get('idLigne').updateLabelInput();
+      yud.get('idArret').parentNode.replaceChild(new Selector('idArret'), yud.get('idArret'));
+      yud.get('idArret').updateLabelInput();
+
+      // Then refill selectors and reselect last values.
+      if (subject.value !== '') {
+        yud.get('idLigne').fill(linesCallUrlPrefix + '/?requestId=' + requestId + '&childId=' + subject.value,
+            select, [lastLineValue]);
+        yud.get('idArret').fill(stopsCallUrlPrefix + '/?requestId=' + requestId + '&childId=' + subject.value + '&lineId=' + lastLineValue,
+            select, [lastStopValue]);
+      }
+
+      yue.on('subjectId', 'change', function(event) {
+        if (this.value !== '') {
+          yud.get('idLigne').fill(linesCallUrlPrefix + '/?requestId=' + requestId + '&childId=' + subject.value);
+        } else {
+          yud.get('idLigne').empty();
+          yud.get('idArret').empty();
+        }
+      });
+
+      yue.on('idLigne', 'change', function(event) {
+        if (this.value !== '') {
+          yud.get('idArret').fill(stopsCallUrlPrefix + '/?requestId=' + requestId + '&childId=' + subject.value + '&lineId=' + this.value);
+        } else {
+          yud.get('idArret').empty();
+        }
+      });
+    };
+
+    /**
+     * Problem:
+     * If we choose "no" in the checkbox, then authorise someone to pick up the child and finally change "no" to "yes".
+     * > The person authorised remains visible on screen.
+     *
+     * Solution:
+     * It would work if authorised persons were in a complex type. Unfortunately we don't support collections in
+     * complex types. So this JS.
+     */
+    var initConditions = function() {
+      yue.on(yud.get('estMaternelleElementaireAutorisations_no'), 'click', hideAllAuthorisationListeners);
+      yue.on(yud.get('estMaternelleElementaireAutorisations_yes'), 'click', displaySelectedAuthorisationListener);
+    };
+
+    var hideAllAuthorisationListeners = function() {
+      zct.each(yus.query('.condition-autoriseTiers-filled, .condition-autoriseFrereOuSoeur-filled'), function() {
+        this.className += ' unactive';
+      });
+    };
+
+    var displaySelectedAuthorisationListener = function() {
+      var selector = yud.get('autorisation');
+      var value = selector.options.item(selector.selectedIndex).value;
+
+      switch (value) {
+        case 'AVEC_FRERE_SOEUR':
+          yud.removeClass(yus.query('.condition-autoriseFrereOuSoeur-filled'), 'unactive');
+          break;
+        case 'AVEC_TIERS':
+          yud.removeClass(yus.query('.condition-autoriseTiers-filled'), 'unactive');
+          break;
+      }
+    };
+
     return {
       init: function() {
-        var subject = yud.get('subjectId');
-        var requestId = yud.get("stepForm").id.value;
-
-        // Return if we aren't on step 1
-        if (subject == undefined)
-          return;
-
-        var linesCallUrlPrefix = zenexity.capdemat.contextPath + '/frontoffice/schoolTransportRegistration/transportLines';
-        var stopsCallUrlPrefix = zenexity.capdemat.contextPath + '/frontoffice/schoolTransportRegistration/stops';
-
-        changeLayout();
-
-        // If we come back from step 2, save last line and stop values
-        var lastLineValue, lastStopValue;
-        if (subject.value !== '') {
-          lastLineValue = yud.get('idLigne').value;
-          lastStopValue = yud.get('idArret').value;
+        // Switch on step name.
+        switch (yus.query('div.form', 'request', true).id) {
+          case 'enfant':
+            initTransports();
+            break;
+          case 'autorisations':
+            initConditions();
+            break;
         }
-
-        yud.get('idLigne').parentNode.replaceChild(new Selector('idLigne'), yud.get('idLigne'));
-        yud.get('idLigne').updateLabelInput();
-        yud.get('idArret').parentNode.replaceChild(new Selector('idArret'), yud.get('idArret'));
-        yud.get('idArret').updateLabelInput();
-
-        // Then refill selectors and reselect last values
-        if (subject.value !== '') {
-          yud.get('idLigne').fill(linesCallUrlPrefix + '/?requestId=' + requestId + '&childId=' + subject.value,
-              select, [lastLineValue]);
-          yud.get('idArret').fill(stopsCallUrlPrefix + '/?requestId=' + requestId + '&childId=' + subject.value + '&lineId=' + lastLineValue,
-              select, [lastStopValue]);
-        }
-
-        yue.on('subjectId', 'change', function(event) {
-          if (this.value !== '') {
-            yud.get('idLigne').fill(linesCallUrlPrefix + '/?requestId=' + requestId + '&childId=' + subject.value);
-          } else {
-            yud.get('idLigne').empty();
-            yud.get('idArret').empty();
-          }
-        });
-
-        yue.on('idLigne', 'change', function(event) {
-          if (this.value !== '') {
-            yud.get('idArret').fill(stopsCallUrlPrefix + '/?requestId=' + requestId + '&childId=' + subject.value + '&lineId=' + this.value);
-          } else {
-            yud.get('idArret').empty();
-          }
-        });
-
       }
     };
 

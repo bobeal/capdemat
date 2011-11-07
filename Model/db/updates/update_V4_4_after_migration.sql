30cd9366a5b2235c9bed6bde4b4250d02617780b
Benoit Orihuela <bor@zenexity.fr>
2011-11-07 19:16:58 +0100

    [Fix] Ordering of SQL instructions between 4.4 and 4.4.2 (cherry picked from commit 4d63050ec00454b652daa82ef61df77818909aa3)

--- Model/db/updates/update_V4_4_after_migration.sql
+++ Model/db/updates/update_V4_4_after_migration.sql
@@ -22,3 +22,12 @@ delete from request where request_type_id in
     (select id from request_type where label = 'VO Card' or label = 'Home Folder Modification');
 
 drop table history_entry;
+
+-- Delete VOCR / HFMR oldies
+
+DELETE FROM requirement where request_type_id in (select id FROM request_type where label = 'VO Card' or label = 'Home Folder Modification');
+DELETE FROM forms where request_type_id in (select id FROM request_type where label = 'VO Card' or label = 'Home Folder Modification');
+DELETE FROM request_type where label = 'VO Card' or label = 'Home Folder Modification';
+
+DROP TABLE vo_card_request ;
+DROP TABLE home_folder_modification_request ;

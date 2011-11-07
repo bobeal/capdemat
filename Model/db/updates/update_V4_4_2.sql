4d63050ec00454b652daa82ef61df77818909aa3
Benoit Orihuela <bor@zenexity.fr>
2011-11-07 19:16:58 +0100

    [Fix] Ordering of SQL instructions between 4.4 and 4.4.2

--- Model/db/updates/update_V4_4_2.sql
+++ Model/db/updates/update_V4_4_2.sql
@@ -5,11 +5,3 @@ UPDATE adult set name_of_use = null where name_of_use = '';
 
 ALTER TABLE user_action ALTER COLUMN note TYPE varchar(1024);
 
--- Delete VOCR / HFMR oldies
-
-DELETE FROM requirement where request_type_id in (select id FROM request_type where label = 'VO Card' or label = 'Home Folder Modification');
-DELETE FROM forms where request_type_id in (select id FROM request_type where label = 'VO Card' or label = 'Home Folder Modification');
-DELETE FROM request_type where label = 'VO Card' or label = 'Home Folder Modification';
-
-DROP TABLE vo_card_request ;
-DROP TABLE home_folder_modification_request ;

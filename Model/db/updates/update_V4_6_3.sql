2f0425760f67a08f45f18b1ead33052b8e9a4692
Benoit Orihuela <bor@zenexity.com>
2012-01-30 15:04:12 +0100

    Add CG95's specific migration for yet another old DB

--- Model/db/updates/update_V4_6_3.sql
+++ Model/db/updates/update_V4_6_3.sql
@@ -1,1 +1,4 @@
 alter table individual_mapping drop column home_folder_mapping_index;
+
+-- old format found in CG95's prod logs : force migrate to Full Copy
+UPDATE death_details_request SET format = 'FULL_COPY'  Where format = 'ExtractWithoutRelationship';

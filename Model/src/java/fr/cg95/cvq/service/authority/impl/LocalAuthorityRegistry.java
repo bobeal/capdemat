26fc815e6a6745b894401d872929a67ddd1ae557
Maxime Calmels <mca@zenexity.com>
2012-07-12 16:11:21 +0200

    [Fix][Core] #5154 information message encoding

--- Model/src/java/fr/cg95/cvq/service/authority/impl/LocalAuthorityRegistry.java
+++ Model/src/java/fr/cg95/cvq/service/authority/impl/LocalAuthorityRegistry.java
@@ -4,12 +4,16 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Transparency;
 import java.awt.image.BufferedImage;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.lang.reflect.Method;
 import java.math.BigInteger;
 import java.net.URI;
@@ -419,10 +423,11 @@ public class LocalAuthorityRegistry
         try {
             if (!assetsFile.exists())
                 assetsFile.createNewFile();
-            FileOutputStream fos = new FileOutputStream(assetsFile);
-            fos.write(data);
-            fos.flush();
-            fos.close();
+            Writer out = new BufferedWriter(new OutputStreamWriter(
+                    new FileOutputStream(assetsFile), "UTF-8"));
+            out.write(new String(data,"UTF-8"));
+            out.flush();
+            out.close();
         } catch (FileNotFoundException e) {
             logger.error("saveLocalAuthorityResource() failel !" + e.getMessage());
             throw new CvqException(e.getMessage());
@@ -444,10 +449,11 @@ public class LocalAuthorityRegistry
         try {
             if (!assetsFile.exists())
                 assetsFile.createNewFile();
-            FileOutputStream fos = new FileOutputStream(assetsFile);
-            fos.write(data);
-            fos.flush();
-            fos.close();
+            Writer out = new BufferedWriter(new OutputStreamWriter(
+                    new FileOutputStream(assetsFile), "UTF-8"));
+            out.write(new String(data,"UTF-8"));
+            out.flush();
+            out.close();
         } catch (FileNotFoundException e) {
             logger.error("saveLocalAuthorityResource() failel !" + e.getMessage());
             throw new CvqException(e.getMessage());

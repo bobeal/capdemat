414022bc142a4838cd422274d2925456c15a22fe
Benoit Orihuela <bor@zenexity.com>
2012-09-16 10:31:29 +0200

    [Fix] Specific SQL update script to synchronize document types ids

---
+++ Model/db/updates/SAINTOUEN-SPECIFIC-006-maj_type_pjs.sql
@@ -0,0 +1,7 @@
+update document_type set type = 65 where name = 'Attestation Preinscription Creche';
+update document_type set type = 64 where name = 'Attestation Honneur Pensions Alimentaires';
+update document_type set type = 63 where name = 'Attestation Pole Emploi';
+update document_type set type = 62 where name = 'Attestation Retraite Complementaire';
+update document_type set type = 61 where name = 'Attestation Annuelle Retraite';
+update document_type set type = 60 where name = 'Autorisation Prelevement';
+update document_type set type = 59 where name = 'Justificatif Divorce';
